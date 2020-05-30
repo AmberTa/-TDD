f8bb387fac24dd2159c0a6b97f4974dad5fc00ec
tangkexin <1738039067@qq.com>
2020-05-30 20:40:34 +0800

    bug

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 application = get_wsgi_application()
+
+sys.path.append('/usr/local/lib/python2.7/site-packages')
