f631de05ed7db1384d63cf168f59dd3fb90d3658
tangkexin <1738039067@qq.com>
2020-06-01 15:55:03 +0800

    bbbb

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os,sys
 
-from django.core.wsgi import get_wsgi_application
-
 sys.path.append('/usr/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+
 
 application = get_wsgi_application()
